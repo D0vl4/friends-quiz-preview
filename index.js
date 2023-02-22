11010993512b60ab7af0f97ac25ea5ed7eed2a4a
Vlado <116637386+D0vl4@users.noreply.github.com>
2023-02-22 14:32:46 +0100

    DEMO quiz v0.5

---
+++ index.js
@@ -0,0 +1,27 @@
+// script for correct answer
+
+const correct = document.getElementById("main-button");
+correct.addEventListener("click", saygood);
+
+function saygood() {
+  document.getElementById("result").innerHTML = "Good job 😊";
+}
+
+// Script for wrong answer
+
+const incorrect = document.getElementById("main-button-two");
+incorrect.addEventListener("click", saybad);
+
+function saybad() {
+  document.getElementById("result").innerHTML = "Awww, you got it wrong 😢";
+}
+
+// Script for reset
+
+const button = document.getElementById("next-question");
+button.addEventListener("click", restart);
+
+function restart() {
+  document.getElementById("result").innerHTML = " ";
+  alert("Thank you for playing DEMO of the quiz!");
+}
